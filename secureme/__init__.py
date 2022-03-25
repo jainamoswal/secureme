1b6a5b9e9ba0c59555dfb592b39e02715290ccd6
Sourcery AI <>
2022-03-25 10:35:59 +0000

    'Refactored by Sourcery'

--- secureme/__init__.py
+++ secureme/__init__.py
@@ -5,11 +5,10 @@ class top():
 
 def HWID():
     string = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
-    HW_ID = 0
     for i in string:
         if i.isdigit() == True:
-            HW += int(i) 
-    return HW_ID
+            HW += int(i)
+    return 0
 
 def encrypt(Text, Method="Length", Password="22"):
     args = ["HWID", "Length", "Password", "Date", "Month", "Year", "Hour"]
@@ -30,10 +29,10 @@ def encrypt(Text, Method="Length", Password="22"):
     if Method not in args:
         print(f"You have to pass one from {args} to encrypt or leave it blank.")
         sys.exit()
-    encrypted = ''
-    for i in Text :
-        encrypted += top._alphabet[int((top._alphabet.find(i) + key) % len(top._alphabet))]
-    return encrypted
+    return ''.join(
+        top._alphabet[int((top._alphabet.find(i) + key) % len(top._alphabet))]
+        for i in Text
+    )
     
 def decrypt(Text, Method="Length", Password="22"):
     args = ["HWID", "Length", "Password", "Date", "Month", "Year", "Hour"]
@@ -54,7 +53,7 @@ def decrypt(Text, Method="Length", Password="22"):
     if Method not in args:
         print(f"You have to pass one from {args} to encrypt or leave it blank.")
         sys.exit()
-    encrypted = ''
-    for i in Text :
-        encrypted += top._alphabet[int((top._alphabet.find(i) + key) % len(top._alphabet))]
-    return encrypted
+    return ''.join(
+        top._alphabet[int((top._alphabet.find(i) + key) % len(top._alphabet))]
+        for i in Text
+    )
